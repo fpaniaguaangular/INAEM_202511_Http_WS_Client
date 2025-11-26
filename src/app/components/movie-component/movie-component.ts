513bd8c9568367baf7330343d8f1c9896aa3c861
fpaniaguaangular <fernando.paniagua.angular@gmail.com>
2025-11-26 18:17:08 +0100

    Refresco

--- src/app/components/movie-component/movie-component.ts
+++ src/app/components/movie-component/movie-component.ts
@@ -1,16 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { HttpOmdbService } from '../../services/http-omdb-service';
+import { IMovie } from '../../interfaces/imovie';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './movie-component.html',
   styleUrl: './movie-component.css',
 })
 export class MovieComponent {
   private httpOmdbService = inject(HttpOmdbService);
+  public movie : IMovie | any;
+  public movieTitle : string = "";
+  private cdr = inject(ChangeDetectorRef);
   constructor() {
-    this.httpOmdbService.getMovieByTitle('Star Wars').subscribe((data)=>{
+  }
+  public getMovieByTitle(){
+    this.httpOmdbService.getMovieByTitle(this.movieTitle).subscribe((data)=>{
+      this.movie=data;
+      this.movieTitle="";
+      this.cdr.detectChanges();
       console.log(data);
     });
   }
